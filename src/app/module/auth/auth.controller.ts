3d5afd1d39095168e8a00a20b5a5b3bb49c7e57f
Rasel Shikder <rasel.sikder777.rk@gmail.com>
2025-10-22 03:04:13 +0600

    added cookies in browser after login router

--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -4,21 +4,37 @@ import sendResponse from "../../shared/sendResponse";
 import { authServices } from "./auth.service";
 
 // Creat paitent
-const createPatient = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    
-    const result = await authServices.login(req.body.email, req.body.password);
-    console.log("result before sendResponse:", result);
+const login = catchAsync(
+    async (req: Request, res: Response, next: NextFunction) => {
 
-    sendResponse(res, {
-      statusCode: 200,
-      success: true,
-      message: "User successfully logged in",
-      data: result,
-    });
-  }
+        const result = await authServices.login(req.body.email, req.body.password);
+        const { accessToken, refreshToken, needPasswordChange } = result
+        console.log("result before sendResponse:", result);
+        // Setting accessToken in cookies
+        res.cookie("accessToken", accessToken, {
+            secure: true,
+            httpOnly: true,
+            sameSite: "none",
+            maxAge: 1000 * 60 * 60
+        })
+        // Setting refreshToken in cookies
+        res.cookie("refreshToken", refreshToken, {
+            secure: true,
+            httpOnly: true,
+            sameSite: "none",
+            maxAge: 1000 * 60 * 60 * 24 * 30
+        })
+        sendResponse(res, {
+            statusCode: 200,
+            success: true,
+            message: "User successfully logged in",
+            data: {
+              needPasswordChange
+            },
+        });
+    }
 );
 
-export const usercontroller = {
-  createPatient,
+export const authcontroller = {
+    login,
 };
--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
+import { authcontroller } from "./auth.controller";
 
 const router = Router()
 
-router.post("/login", )
+router.post("/login", authcontroller.login)
